d7c6edefe873bdefb06309c7c964a5c2e13db80a
mulderandbuffy <laurahenry2409@gmail.com>
2021-01-25 15:39:16 +0000

    Chapter 4 25/01/2021 (End of Chapter)

--- rango/views.py
+++ rango/views.py
@@ -12,4 +12,11 @@ def index(request):
     return(render(request, 'rango/index.html', context=context_dict))
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    #Construct a dictionary to pass the template engine as its context
+    #Note the key boldmessage matches to the variable in the html doc!
+    context_dict = {'boldmessage': 'This tutorial has been put together by Laura Henry'}
+
+    #Return rendered response
+    #We make use of the shortcut function to make lives easier
+    #First parameter is the template
+    return(render(request, 'rango/about.html', context=context_dict))
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -118,7 +120,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+#Media Files (Dynamic)
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
