382f63ab91121946416f3747bd0fcc48be657d7a
mulderandbuffy <laurahenry2409@gmail.com>
2021-01-26 18:13:49 +0000

    Chapter 5 26/01/2021 End of Chapter

--- populate_rango.py
+++ populate_rango.py
@@ -1,5 +1,5 @@
 import os
-os.envision.setdefault('DJANGO_SETTINGS_MODULE',
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 'tango_with_django_project.settings')
 
 import django
@@ -18,13 +18,44 @@ def populate():
         {'title':'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
         {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'},
         {'title':'How to Tango with Django','url':'http://www.tangowithdjango.com/'} ]
-    ]
+    
     other_pages = [
         {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask', 'url':'http://flask.pocoo.org'} ]
-    ]
-    cats =
-        {'Python': {'pages': {python_pages},
-        'Django': {'pages': djamgo_pages},
-        'Other Framerworks': {'pages': other_pages}
     
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64 },
+        'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+        'Other Framerworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+
+    #add more pages and categories to above dictionaries
+
+    #add each category then add the associated pages
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
+        for p in cat_data['pages']:
+            add_page(c, p['title'], p['url'])
+
+    #print
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'-{c}: {p}')
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views = views
+    p.save()
+    return p
+
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views
+    c.likes
+    c.save()
+    return c
+    
+#execution
+if __name__ == '__main__':
+    print('Starting Rango population script...')
+    populate()
+    print('Rango database populated')
